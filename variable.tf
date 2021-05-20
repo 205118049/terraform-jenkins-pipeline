65ad29f5ea8eb502558e0439b3f074faf76cd5a7
Praful Waghe <praful2428>
2021-05-20 11:53:31 +0530

    second commit

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "region" { default = "ap-southeast-2" }
 variable "instance_type" { default = "t2.micro" }
 variable "key_name" { default = "kenopsy" }
 
- variable "ssh_private_key_file" { default = "*/kenopsy.pem"}
+ variable "ssh_private_key_file" { default = "kenopsy.pem"}
  
  locals {
     private_key = file(var.ssh_private_key_file)
